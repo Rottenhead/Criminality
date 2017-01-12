90dd5cf92367db313cfdb7debb33330812fb8745
Konstantin Petrov <konstload@gmail.com>
2017-01-12 20:22:50 +0300

    Добавил Список преступлений(RecyclerView CrimeListFragment), настроил его отображение (list_item_crime.xml) пока список выводит только тост при клике + Singleton(CrimeLab) + абстрактный клас создания фрагментов(SingleFragmentActivity)

--- app/src/main/java/com/example/satan/criminality/CrimeListActivity.java
+++ app/src/main/java/com/example/satan/criminality/CrimeListActivity.java
@@ -2,10 +2,9 @@ package com.example.satan.criminality;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
-
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
